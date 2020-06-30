35cc3a376e6615ba34f51e500eb2f60bfc97e9b2
Lawrence <Caulker>
2020-06-30 18:31:12 -0400

    initial setup

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./images/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./images/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./images/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./images/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+]
