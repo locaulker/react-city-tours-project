2930451d7b8bd996fb42e27d8272f06aaf0ac8d6
Lawrence <locaulker@gmail.com>
2020-07-01 06:48:06 -0400

    initiated TourList component

--- src/components/TourList/Tourlist.js
+++ src/components/TourList/Tourlist.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react"
 import Tour from "../Tour/Tour"
+import "./TourList.scss"
+import { tourData } from "../../tourData"
 
 class Tourlist extends Component {
+  state = {
+    tours: tourData
+  }
+
+  removeTour = id => {
+    console.log(id)
+  }
+
   render() {
+    // console.log(this.state.tours)
+
+    const { tours } = this.state
+
     return (
-      <h1>
-        Hello from TourList
-        <Tour />
-      </h1>
+      <section className="tourlist">
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
     )
   }
 }
